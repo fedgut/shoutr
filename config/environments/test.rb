f094c0f7ba3106171b7c1375db6733aab4c8abb2
fedgut <fedgut@gmail.com>
2021-02-22 17:23:43 -0600

    Add mailer host to dev and test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'changed_in_test_env@example.com' }
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
