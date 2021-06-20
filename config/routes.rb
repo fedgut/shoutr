fb19f1f0d8ec454e84f40599e47dbce24221fa0d
fedgut <fedgut@gmail.com>
2021-06-20 14:53:25 -0500

    Display shouts

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
   resources :passwords, controller: 'clearance/passwords', only: %i[create new]
   resource :session, only: :create
 
+  resources :shouts, only: %i[create]
+
   resources :users, only: :create do
     resource :password,
              controller: 'clearance/passwords',
              only: %i[edit update]
   end
-
+  
   get '/sign_in' => 'sessions#new', as: 'sign_in'
-  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
   get '/sign_up' => 'users#new', as: 'sign_up'
+  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
 end
