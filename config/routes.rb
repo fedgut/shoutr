fd8b720d4951e3f4af4e29c2dca6a5dec5277c09
fedgut <fedgut@gmail.com>
2021-06-20 14:18:39 -0500

    create dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'home#show'
-  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
-  resource :session, only: [:create]
-
-  resources :users, only: [:create] do
-    resource :password,
-      controller: "clearance/passwords",
-      only: [:edit, :update]
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboard#show', as: :signed_in_root
   end
 
-  get "/sign_in" => "sessions#new", as: "sign_in"
-  delete "/sign_out" => "sessions#destroy", as: "sign_out"
-  get "/sign_up" => "users#new", as: "sign_up"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#show'
+  resources :passwords, controller: 'clearance/passwords', only: %i[create new]
+  resource :session, only: :create
+
+  resources :users, only: :create do
+    resource :password,
+             controller: 'clearance/passwords',
+             only: %i[edit update]
+  end
+
+  get '/sign_in' => 'sessions#new', as: 'sign_in'
+  delete '/sign_out' => 'sessions#destroy', as: 'sign_out'
+  get '/sign_up' => 'users#new', as: 'sign_up'
 end
