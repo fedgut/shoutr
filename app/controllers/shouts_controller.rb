b3a02faa89515dc1a39f640e430bb500a63c5d93
fedgut <fedgut@gmail.com>
2021-06-20 14:53:41 -0500

    Display shouts

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,20 @@
+class ShoutsController < ApplicationController
+  def create
+    shout = current_user.shouts.create(shout_params)
+    redirect_to root_path, redirect_options_for(shout)
+  end
+
+  private
+
+  def shouts_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_options_for(shout)
+    if shout.persisted?
+      flash.notice = 'Success!'
+    else
+      flash.alert = 'Unable to shout'
+    end
+  end
+end
